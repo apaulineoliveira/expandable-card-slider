9b0d483551c39694287a176fc04d0fd71352f873
apaulineoliveira <apaulineoliveira@gmail.com>
2023-01-21 23:50:13 -0300

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-$(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
-  });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
-  
+const items = document.querySelectorAll('.item')
+
+items.forEach(item =>{
+  item.addEventListener('click',() => {
+      removeActionClasses()
+      item.classList.add('active')
+  })
+})
+
+function removeActionClasses(){
+    items.forEach(item =>{
+        item.classList.remove('active')
+    })
+}
